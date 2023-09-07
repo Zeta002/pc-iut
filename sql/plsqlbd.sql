7fe868ac1ed1bdc830ceb4f7240ba6c95a80b391
Zeta002 <angeloportelli05@gmail.com>
2023-09-07 17:28:01 +0200

    fin de cours

--- sql/plsqlbd.sql
+++ sql/plsqlbd.sql
@@ -16,9 +16,38 @@ END;
 
 DECLARE
 
-test VARCHAR2(25) DEFAULT 'NOT DEFINED';
-/* Nom_Et ETUDIANT.Nom_Et%TYPE DEFAULT ‘RUBINI’ ; */ -- type dynamique
+test VARCHAR2(25) DEFAULT 'NOT DEFINED'; -- variable
+Nom_Et ETUDIANT.Nom_Et%TYPE DEFAULT ‘RUBINI’; -- type dynamique
+uneVariableConstante NUMBER(6,2):= 5000; -- constante
+test ETDUIANT%ROWTYPE; -- type composé
+test VARCHAR2(25) := Un_Etudiant.Nom_Et; -- accès à la valeur d'une composante
 
 BEGIN
     NULL;
+    /*SELECT 
+    INTO -- variable repectrice
+     FROM WHERE*/
+END;
+
+-- Afficher la moyenne des notes de test par matière.
+DECLARE
+    CURSOR MoyMat IS
+    SELECT Code, AVG(Moy_Test) Moy
+    FROM NOTATION
+    GROUP BY Code;
+
+Une_Ligne MoyMat%ROWTYPE;
+
+BEGIN
+    OPEN MoyMat;
+    FETCH MoyMat INTO Une_Ligne;
+
+    WHILE MoyMat%FOUND
+    LOOP
+    DBMS_OUTPUT.PUT_LINE(Une_Ligne.Code || ' ' ||
+    Trunc(Une_Ligne.Moy, 2));
+    FETCH MoyMat INTO Une_Ligne;
+    END LOOP;
+    
+    CLOSE MoyMat;
 END;
